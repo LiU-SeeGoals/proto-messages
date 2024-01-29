c31a9394d274ba6ac86ae21bb70b4e94a1410daa
woshyy <christopher.waatz@gmail.com>
2024-01-29 18:48:07 +0100

    Remade the build file and added build files and removed unnecessary build files

--- build_go.sh
+++ build_go.sh
@@ -1,26 +1,31 @@
-
 #!/bin/bash
 
 # Define the root directory where the script is located
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Function to generate Go code from proto files in a directory
 generate_go_code() {
-    local dir_name="$1"
-    local dir_path="${SCRIPT_DIR}/${dir_name}"
-    for PROTO_FILE in "${dir_path}"/*.proto; do
-        # Generate Go code in the directory of the script
-        protoc --proto_path="${dir_path}" --go_out="${SCRIPT_DIR}/proto" "${PROTO_FILE}"
-    done
+	local dir_name="$1"
+	local dir_path="${SCRIPT_DIR}/${dir_name}" # Corrected path
+	local output_dir="${SCRIPT_DIR}/go/"       # Output directory
+
+	# Ensure the output directory exists
+	mkdir -p "${output_dir}"
+
+	# Check if the source directory exists
+	if [ -d "${dir_path}" ]; then
+		for PROTO_FILE in "${dir_path}"/*.proto; do
+			# Generate Go code in the output directory
+			protoc --proto_path="${dir_path}" --go_out="${output_dir}" "${PROTO_FILE}"
+		done
+	else
+		echo "Warning: Directory ${dir_path} does not exist."
+	fi
 }
 
-# Generate Go code for proto files in the action directory
+# Generate Go code for proto files in the action, grsim, and ssl_vision directories
 generate_go_code "robot_action"
-
-# Generate Go code for proto files in the grsim directory
 generate_go_code "grsim"
-
-# Generate Go code for proto files in the ssl_vision directory
 generate_go_code "ssl_vision"
 
-echo "Proto files built in ${SCRIPT_DIR}"
+echo "Proto files built in ${SCRIPT_DIR}/go"
