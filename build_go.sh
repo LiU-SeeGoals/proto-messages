6ddae7f85394c0fa760768c418f4a68e3c3a0084
woshyy <christopher.waatz@gmail.com>
2024-02-07 20:26:12 +0100

    Adding the new protofiles to the remote go package

--- build_go.sh
+++ build_go.sh
@@ -6,25 +6,26 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # Function to generate Go code from proto files in a directory
 generate_go_code() {
 	local dir_name="$1"
-	local dir_path="${SCRIPT_DIR}/${dir_name}" # Corrected path
-	local output_dir="${SCRIPT_DIR}/go/"       # Output directory
+	local dir_path="${SCRIPT_DIR}/${dir_name}" # Corrected to include subdirectory
+	local output_dir="${SCRIPT_DIR}/go"        # Output in subdirectories
 
 	# Ensure the output directory exists
 	mkdir -p "${output_dir}"
 
 	# Check if the source directory exists
 	if [ -d "${dir_path}" ]; then
-		for PROTO_FILE in "${dir_path}"/*.proto; do
-			# Generate Go code in the output directory
-			protoc --proto_path="${dir_path}" --go_out="${output_dir}" "${PROTO_FILE}"
-		done
+		# Find all .proto files and generate Go code
+		find "${dir_path}" -name '*.proto' -exec protoc --proto_path="${SCRIPT_DIR}" --go_out="${output_dir}" --go_opt=paths=source_relative {} +
 	else
 		echo "Warning: Directory ${dir_path} does not exist."
 	fi
 }
 
-# Generate Go code for proto files in the action, grsim, and ssl_vision directories
+# Generate Go code for proto files in the grsim, robot_action, and ssl_vision directories
+generate_go_code "simulation"
 generate_go_code "robot_action"
+generate_go_code "parsed_vision"
+generate_go_code "ssl_vision"
 generate_go_code "grsim"
 
 echo "Proto files built in ${SCRIPT_DIR}/go"
--- go/go.mod
+++ go/go.mod
@@ -3,3 +3,5 @@ module github.com/LiU-SeeGoals/proto-messages/go
 go 1.21.2
 
 require google.golang.org/protobuf v1.32.0
+
+require github.com/LiU-SeeGoals/proto-messages v0.1.2 // indirect
