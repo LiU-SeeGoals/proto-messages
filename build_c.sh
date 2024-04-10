1dcd6115b650212fefc9ba1c17776f85c633241a
Axel <axel.hammarberg@gmail.com>
2024-04-10 19:15:42 +0200

    Fixed c generator

--- build_c.sh
+++ build_c.sh
@@ -3,25 +3,20 @@
 # Define the root directory where the script is located
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-# Define the directories containing the .proto files
-SSL_VISION_DIR="${ROOT_DIR}/ssl_vision"
-ACTION_DIR="${ROOT_DIR}/robot_action"
-
-# Create the directories if they don't already exist
-mkdir -p "${SSL_VISION_DIR}"
-mkdir -p "${ACTION_DIR}"
-
+# Function to generate C code from proto files in a directory
 # Function to generate C code from proto files in a directory
 generate_c_code() {
-	local dir_path="$1"
+	local dir_path="${ROOT_DIR}/$1"
+	local out_dir="${ROOT_DIR}/proto_c/$1"
+	mkdir -p "${out_dir}"
 	for PROTO_FILE in "${dir_path}"/*.proto; do
-		protoc-c --c_out="${dir_path}" --proto_path="${dir_path}" "${PROTO_FILE}"
+		protoc-c --c_out="${out_dir}" --proto_path="${dir_path}" --proto_path="$PWD" "${PROTO_FILE}"
 	done
 }
 
 # Generate C code for each directory
 echo "Generating C code for SSL Vision proto files..."
-generate_c_code "${SSL_VISION_DIR}"
+generate_c_code "parsed_vision"
 
 echo "Generating C code for Robot Action proto files..."
-generate_c_code "${ACTION_DIR}"
+generate_c_code "robot_action"
